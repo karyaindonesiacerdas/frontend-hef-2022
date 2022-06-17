25518c6a857fb179a5c039f80a2bfcaf1dbb6147
Kamal Mahmudi <kamalmahmudi@gmail.com>
2022-06-17 09:10:07 +0700

    randomize video on main hall for mobile version

--- components/main-hall/Advertisement.tsx
+++ components/main-hall/Advertisement.tsx
@@ -1,18 +1,21 @@
 import React from "react";
-import { matchYoutubeUrl } from "utils/youtube";
+import { extractYoutubeParams } from "utils/youtube";
 
 const Advertisement = ({ url }: { url?: string }) => {
   const defaultVideo =
     "https://www.youtube.com/watch?v=jS0qVrpKjY4&ab_channel=HospitalEngineeringExpo";
 
-  const youtubeId = matchYoutubeUrl(url || defaultVideo);
+    const { id, list } = extractYoutubeParams(url || defaultVideo);
+    const params = list
+      ? `?listType=playlist&list=${list}&loop=1&index=${Math.floor(Math.random() * 4)}`
+      : `${id}?version=3&autoplay=1&playlist=${id}&loop=1`;
 
   return (
     <iframe
       style={{ aspectRatio: "16/9" }}
       width="100%"
       height="100%"
-      src={`https://www.youtube.com/embed/${youtubeId}?version=3&playlist=${youtubeId}`}
+      src={`https://www.youtube.com/embed/${params}`}
       title="YouTube video player"
       frameBorder="0"
       // allow="autoplay; loop; fullscreen"
